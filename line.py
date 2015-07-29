09596d7e486ef13cb84ba040233af6dc52bad147
Patrick Knisely <patrick_knisely@hotmail.com>
2015-07-29 12:48:25 -0500

    Update line.py
    Add a few comments, start a getter function for line_status

--- line.py
+++ line.py
@@ -1,3 +1,12 @@
 class Line:
     def __init__(self, coin_one, coin_two, coin_three)
         self.
+
+
+    def __get_line_status__(self):
+        return self.line_status
+        
+        
+// Need to have broken or unbroken
+// Need to have changing or not-changing
+
