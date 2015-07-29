44be8060085693010c71606a66739f4db17a7c3b
Patrick Knisely <patrick_knisely@hotmail.com>
2015-07-29 12:45:42 -0500

    Create coin.py
    coin class - first take

---
+++ coin.py
@@ -0,0 +1,18 @@
+import random
+
+class Coin:
+    def __init__(self, side_up = 'Heads'):
+        self.side_up = side_up
+
+    def __flip__(self):
+        random_flip = random.randint(0,1)
+        if random_flip == 0:
+            self.side_up = 'Heads'
+        else:
+            self.side_up = 'Tails'
+
+    def __get_side_up__(self):
+        return self.side_up
+
+    
+    
