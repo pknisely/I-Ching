84885b1a4a822939423b8a04d14e5090623df7f7
Patrick Knisely <patrick_knisely@hotmail.com>
2015-08-03 14:55:57 -0500

    Update ichine.py
    Working on full line integration and hexagram integration; using loop to create list of lines to then create hexagram

--- iching.py
+++ iching.py
@@ -8,10 +8,21 @@ coin_three = coin.Coin()
 
 def main():
     print('Welcome to the iChing.')
-    toss_coins()
-    return_coins()
-    line_one = get_line(coin_one, coin_two, coin_three)
-    print(line_one.__get_type__())
+    print()
+    print('PLACEHOLDER FOR EXPLANATION')
+    print()
+    user_hex = get_hexagram()
+    display_hexagram(user_hex)
+
+def get_hexagram():
+    line_list = []
+
+    for x in range(0, 6):
+        toss_coins()
+        return_coins()
+        temp_line = get_line(coin_one, coin_two, coin_three)
+        line_list.append(temp_line)       
+    hex = hexagram.Hexagram(line_list[0], line_list[1], line_list[2], line_list[3], line_list[4], line_list[5])
 
 def toss_coins():      
     input('Press enter to toss coins')
@@ -20,16 +31,13 @@ def toss_coins():
     coin_three.__flip__()
 
 def return_coins():
-    print(coin_one.__get_side_up__())
-    print(coin_two.__get_side_up__())
-    print(coin_three.__get_side_up__())
+    print('The results of your coin toss is: ', coin_one.__get_side_up__(), coin_two.__get_side_up__(), coin_three.__get_side_up__())
 
 def get_line(coin_one, coin_two, coin_three):
     temp_line = line.Line(coin_one, coin_two, coin_three)
     return temp_line
 
+def displayhexagram(example_hex):
+    print('Temp')
+
 main()
-
-
-
-
