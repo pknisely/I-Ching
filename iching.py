3e7f86be7e76575f30648f7ee2d6347b4744bb8b
Patrick Knisely <patrick_knisely@hotmail.com>
2015-08-03 13:56:49 -0500

    Update iching.py
    Working on integrating line information.  Still have lots of work to do.

--- iching.py
+++ iching.py
@@ -7,27 +7,29 @@ coin_two = coin.Coin()
 coin_three = coin.Coin()
 
 def main():
-    intro_screen()
-
-def intro_screen():
     print('Welcome to the iChing.')
-    input('Press enter to toss coins')
     toss_coins()
     return_coins()
+    line_one = get_line(coin_one, coin_two, coin_three)
+    print(line_one.__get_type__())
 
 def toss_coins():      
+    input('Press enter to toss coins')
     coin_one.__flip__()
     coin_two.__flip__()
     coin_three.__flip__()
-
 
 def return_coins():
     print(coin_one.__get_side_up__())
     print(coin_two.__get_side_up__())
     print(coin_three.__get_side_up__())
 
+def get_line(coin_one, coin_two, coin_three):
+    temp_line = line.Line(coin_one, coin_two, coin_three)
+    return temp_line
 
 main()
+
 
 
 
